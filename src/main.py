865ac725c2a1d113d5fbbdab78d83361885b079f
Justin Huang <xinglongjh@gmail.com>
2021-09-18 22:44:12 -0400

    Made JSON, dropped NAs

--- src/main.py
+++ src/main.py
@@ -13,19 +13,18 @@ def clean_pct(stat: str):
 
 
 # Drop unnecessary columns
-zips = pd.read_csv('csv/zip.csv')
+zips = pd.read_csv('data/zip.csv')
 zips = zips.drop(columns=['zip'])
 
 # Drop unnecessary columns
-returns = pd.read_csv('csv/returns.csv')
+returns = pd.read_csv('data/returns.csv')
 returns = returns.drop(columns=['lag0','lag1','lag2','lag3'])
 
 # Drop NA rows
-esg = pd.read_csv('csv/esg.csv')
-esg = esg.dropna()
+esg = pd.read_csv('data/esg.csv')
 
 # Clean data
-echo = pd.read_csv('csv/echo.csv')
+echo = pd.read_csv('data/echo.csv')
 echo['minority_pct'] = echo['minority_pct'].apply(lambda x: re.sub(r'[%]', '', str(x)))
 problems = ['primary_school','high_school','hs_diploma','some_college','bsba','income_l15','income_15_25','income_25_50','income_50_75','income_p75']
 for problem in problems:
@@ -36,5 +35,7 @@ for problem in problems:
 master = pd.merge(zips, esg, on='ticker')
 master = pd.merge(master, echo, on='ticker')
 master = pd.merge(master, returns, on='ticker')
-
-master.to_csv('csv/master.csv')
+master = master.dropna()
+master.to_csv('data/master.csv', index=False)
+master = master.set_index('longName')
+master.to_json('data/master.json', orient='index')
